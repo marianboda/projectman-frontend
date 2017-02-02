0e096db7a7f0c9b3b18c7e5419c29988b5255c9c
Marian Boda <marianboda@gmail.com>
2017-02-02 02:19:55 +0100

    Added eslint exception for the redux devTools

--- src/store.js
+++ src/store.js
@@ -16,8 +16,8 @@ const reducer = (state = initialState, action) => {
   }
 }
 
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// eslint-disable-next-line no-underscore-dangle
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 const store = createStore(
   reducer,
