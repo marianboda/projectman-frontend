9b77f295c6077647ba438c3849c5b2db8b90dc76
Marian Boda <marianboda@gmail.com>
2017-02-02 02:05:22 +0100

    Added module for graphql access

--- src/actions.js
+++ src/actions.js
@@ -1,9 +1,20 @@
+import * as gql from './gql'
+
 export const tasksLoaded = (data) => ({
   type: 'TASKS_LOADED',
   payload: data,
 })
 
-export const setTask = (data) => ({
-  type: 'SET_TASK',
+const taskUpdate = (data) => ({
+  type: 'TASK_UPDATE',
   payload: data,
 })
+
+export const setTask = (data) => (dispatch) => {
+  dispatch({
+    type: 'SET_TASK',
+    payload: data,
+  })
+  gql.setTask(data)
+    .then((res) => console.log('res received', res))
+}
---
+++ src/gql.js
@@ -0,0 +1,22 @@
+const apiConfig = {
+  method: 'POST',
+  headers: new Headers({ 'Content-Type': 'application/json' }),
+  body: '{"query": "mutation { setTask(task: {name: \\"aaa-aaa\\"}) {id} }"}',
+}
+
+export const query = (q) => {
+  const queryStr = JSON.stringify({ query: q })
+  console.log('q = ', queryStr)
+  return fetch('/graphql', { ...apiConfig, body: queryStr })
+}
+
+const quoteVal = (val) => {
+  return JSON.stringify(val)
+}
+
+export const setTask = (rec) => {
+  const select = ['id', 'name'].join(',')
+  const vals = Object.keys(rec).map(i => `${i}: ${quoteVal(rec[i])}`).join(', ')
+  const q = `mutation { setTask(task: {${vals}}) { ${select} } }`
+  return query(q)
+}
