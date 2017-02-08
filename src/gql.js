0ea562525d683ce0fce56c0039d5ac2b3b6605d3
Marian Boda <marianboda@gmail.com>
2017-02-09 00:59:36 +0100

    Replaced concated queries with Apollo versions

--- src/gql.js
+++ src/gql.js
@@ -1,44 +1,25 @@
-import client from './ApolloClient'
 import gql from 'graphql-tag'
+import client from './ApolloClient'
 
-const apiConfig = {
-  method: 'POST',
-  headers: new Headers({ 'Content-Type': 'application/json' }),
-}
-
-export const query = (q) => {
-  const queryStr = JSON.stringify({ query: q })
-  console.log('q = ', queryStr)
-  return fetch('/graphql', { ...apiConfig, body: queryStr })
-}
-
-export const mutate = (mutation) => {
-  const q = {
-    mutation: gql`mutation { setTask(task: { name: "${new Date().getSeconds()}"}) { id, name, state_id } }`,
-    refetchQueries: [{ query: gql`query taskquery {
-        tasks {
-          id
-          name
-          state_id
-          project_id
-          priority
-          __typename
-        }
-      }
-      ` }],
+const getTasksQuery = gql`query taskquery {
+  tasks {
+    id
+    name
+    state_id
+    project_id
+    priority
+    __typename
   }
+}`
 
-  return client.mutate(q)
-}
-
-const quoteVal = (val) => {
-  return JSON.stringify(val)
-}
+const setTaskMutation = gql`mutation setTask($task: TaskInput) {
+  setTask(task: $task) { id, name, state_id }
+}`
 
 export const setTask = (rec) => {
-  const select = ['id', 'name', 'state_id'].join(', ')
-  const vals = Object.keys(rec).map(i => `${i}: ${quoteVal(rec[i])}`).join(', ')
-  const q = `mutation { setTask(task: {${vals}}) { ${select} } }`
-  // return query(q)
-  return mutate()
+  return client.mutate({
+    mutation: setTaskMutation,
+    variables: { task: rec },
+    refetchQueries: [{ query: getTasksQuery }],
+  })
 }
