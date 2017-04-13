09c51101ffa7b841489f485356282e133a40a314
Marian Boda <marianboda@gmail.com>
2017-04-13 02:04:26 +0200

    Added animation to the task list

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -4,7 +4,7 @@ import { Link } from 'redux-little-router'
 import gql from 'graphql-tag'
 import { graphql, compose } from 'react-apollo'
 import R from 'ramda'
-
+import FlipMove from 'react-flip-move'
 const taskQuery = gql`query taskQuery { tasks { id name state_id project {id name} priority } }`
 
 class TaskList extends React.Component {
@@ -29,23 +29,25 @@ class TaskList extends React.Component {
                   </tr>
                 </thead>
                 <tbody>
-                  {sort(tasks).map(i => {
-                    return (
-                      <tr key={i.id} className={`tr-task tr-task-state-${i.state_id}`}>
-                        <td>
-                          <input
-                            type="checkbox"
-                            checked={i.state_id === 5}
-                            onChange={((j) => () => onTaskCheck(j))(i)}
-                          />
-                        </td>
-                        <td><Link href={`/tasks/${i.id}`}>{i.name}</Link></td>
-                        <td>{i.state_id}</td>
-                        <td>{i.project && i.project.name}</td>
-                        <td className="task-priority">{i.priority}</td>
-                      </tr>
-                    )
-                  })}
+                  <FlipMove duration={350} easing="ease">
+                    {sort(tasks).map(i => {
+                      return (
+                        <tr key={i.id} className={`tr-task tr-task-state-${i.state_id}`}>
+                          <td>
+                            <input
+                              type="checkbox"
+                              checked={i.state_id === 5}
+                              onChange={((j) => () => onTaskCheck(j))(i)}
+                            />
+                          </td>
+                          <td><Link href={`/tasks/${i.id}`}>{i.name}</Link></td>
+                          <td>{i.state_id}</td>
+                          <td>{i.project && i.project.name}</td>
+                          <td className="task-priority">{i.priority}</td>
+                        </tr>
+                      )
+                    })}
+                  </FlipMove>
                 </tbody>
               </table>
             </div>)
